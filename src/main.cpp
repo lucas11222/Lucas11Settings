70902e74e7158889343056e6e8246411c943348d
BuildTools <altamiranolucas2013@gmail.com>
2025-01-25 14:59:44 +0100

    Configure IMGUI and add sometgins

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
  */
 #include <Geode/Geode.hpp>
 #include <Geode/ui/GeodeUI.hpp>
+#include <gdutilsdevs.gdutils/include/RateEvent.hpp>
+#include <gdutilsdevs.gdutils/include/Types.hpp>
+#include <imgui-cocos.hpp>
+#include <geode.custom-keybinds/include/Keybinds.hpp>
 /**
  * Brings cocos2d and all Geode namespaces to the current scope.
  */
@@ -38,11 +42,16 @@ class $modify(MyMenuLayer, MenuLayer) {
 		 * We call the original init function so that the
 		 * original class is properly initialized.
 		 */
+
 		
-		
-		if (!MenuLayer::init()) {
-			return false;
-		}    	
+        if (!MenuLayer::init()) return false;
+          static bool initialized = false;
+        if (!initialized) {
+              ImGuiCocos::get()
+                  .setup(setup).draw(draw)
+                  .setVisible(false);
+              initialized = true;
+        }  	
 		/**
 		 * You can use methods from the `geode::log` namespace to log messages to the console,
 		 * being useful for debugging and such. See this page for more info about logging:
@@ -55,7 +64,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 		 * https://docs.geode-sdk.org/tutorials/buttons
 		*/
 		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_longBtn06_001.png"),
+			CCSprite::createWithSpriteFrameName("GJ_epicCoin3_001.png"),
 			this,
 			/**
 			 * Here we use the name we set earlier for our modify class.
@@ -83,18 +92,55 @@ class $modify(MyMenuLayer, MenuLayer) {
 		 * https://docs.geode-sdk.org/tutorials/layouts
 		*/
 		menu->updateLayout();
-
 		/**
 		 * We return `true` to indicate that the class was properly initialized.
 		 */
+		auto& io = ImGui::GetIO();
+    	auto fontPath = geode::Mod::get()->getResourcesDir() / "Roboto-Regular.ttf";
+    	io.Fonts->AddFontFromFileTTF(fontPath.string().c_str(), 16.0f);
 		return true;
 	}
+	void draw() {
+    /**
+     * This function should be used for drawing ImGui widgets.
+     * You can put any ImGui code here, and it will be rendered on the screen.
+     */
+    	ImGui::Begin("Hello, ImGui!");
 
+    	ImGui::Text("This is a simple ImGui window.");
+    	ImGui::Text("You can put any ImGui widgets here.");
+
+    	if (ImGui::Button("Close")) {
+        	/* This will hide our ImGui interface. */
+        	ImGuiCocos::get().toggle();
+    }
+
+    ImGui::End();
+	}	
 	/**
 	 * This is the callback function for the button we created earlier.
 	 * The signature for button callbacks must always be the same,
 	 * return type `void` and taking a `CCObject*`.
 	*/
+	$execute {
+    /**
+     * Bringing some namespaces into scope for easier access to classes and functions.
+     */
+    	using namespace geode::prelude;
+    	using namespace keybinds;
+
+    	BindManager::get()->registerBindable({
+        	"open-menu-settings"_spr, /* Keybind ID */
+        	"Open Interface", /* Keybind name */
+        	"Open or close the ImGui interface.", /* Keybind description */
+        	{ Keybind::create(cocos2d::enumKeyCodes::KEY_S, Modifier::Alt) },
+        	"Lucas11Settings" /* Category name (usually the name of your mod) */
+    	});
+    	new EventListener([=](InvokeBindEvent* event) {
+        	if (event->isDown()) ImGuiCocos::get().toggle();
+        	return ListenerResult::Propagate;
+    	}, InvokeBindFilter(nullptr, "open-menu-settings"_spr));
+	}
 	void onMyButton(CCObject*) {
 		geode::openSettingsPopup(Mod::get());
 		if (!Mod::get()->setSavedValue("shown-upload-guidelines", true)) {
@@ -102,7 +148,23 @@ class $modify(MyMenuLayer, MenuLayer) {
         	"Lucas11",
         	"Hello! Please read the README or the description of the mod.",
         	"OK"
+			
     	)->show();
+		EventData data = {
+			true,
+			2,
+			6,
+			1,
+    		EventType::smallChest, // type of notification
+    		"hello world", // notification title
+    		"GJ_editBtn_001.png", // sprite (MUST BE VALID OR WILL CRASH!)
+    		"hi by mod", // level name
+    		"by lucas11", // level creator
+			0,
+			false,	
+			false,	
+		};
+		GDUtils::Events::RateEvent::emit(data);
 }
 
 	}
